44e7710e5f4bf97112c6725e135c21db4c2ef5d2
lucarl07 <07lucarl.dev@gmail.com>
2026-05-27 16:00:54 -0300

    feat(app/(tabs)): Personalizar tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,48 @@
-import { Tabs, Redirect } from 'expo-router';
 import * as SecureStore from 'expo-secure-store';
+import { Tabs, Redirect } from 'expo-router';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TabsLayout() {
   const isAuthenticated = SecureStore.getItem("is_user_logged");
 
   if (isAuthenticated != "true") return <Redirect href="/login" />;
 
-  return <Tabs />;
+  return (
+	<Tabs
+		screenOptions={{
+			headerShown: false,
+			tabBarActiveTintColor: '#35609b',
+		}}
+	>
+		<Tabs.Screen 
+			name='index'
+			options={{
+				title: 'Início',
+				tabBarIcon: ({color}) => <MaterialIcons size={28} name='home' color={color} />,
+			}}
+		/>
+		<Tabs.Screen 
+			name='pagamentos'
+			options={{
+				title: 'Pagamentos',
+				tabBarIcon: ({color}) => <MaterialIcons size={28} name='attach-money' color={color} />, 
+			}}
+		/>
+		<Tabs.Screen 
+			name='usuario'
+			options={{
+				title: 'Usuário',
+				tabBarIcon: ({color}) => <MaterialIcons size={28} name='person' color={color} />,
+			}}
+		/>
+		<Tabs.Screen 
+			name='outros'
+			options={{
+				title: 'Outros',
+				tabBarIcon: ({color}) => <MaterialIcons size={28} name='view-list' color={color} />,
+			}}
+		/>
+	</Tabs>
+  );
 }
 
---
+++ app/(tabs)/usuario.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Usuario() {
+	return (
+		<View>
+			{/*  Me substitua pelo seu componente!  */}
+			<Text>Olá mundo!</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({})
