d0861308b1505d0288bb1c92438cb5f7a01b1b82
N3rdMusical05 <jp3188619@gmail.com>
2026-05-27 23:27:00 -0300

    Construindo a página inicial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Inicio() {
+	const styles = header;
+
 	return (
-		<View>
-			{/*  Me substitua pelo seu componente!  */}
-			<Text>Olá mundo!</Text>
-			<Text>Eu sou a página Início.</Text>
-		</View>
+		    <SafeAreaView style={styles.container}>
+			    <View style={styles.View1}>
+
+					<Text> UNINASSAU SYSTEM!</Text>
+			    </View>
+
+				<ScrollView> style={styles.container2}
+					<Text>Olá, mundo!</Text>
+				</ScrollView>
+		</SafeAreaView>
+
+		
 	)
+
 }
 
-const styles = StyleSheet.create({})
+const header = StyleSheet.create({
+
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		backgroundColor: '#f0f0f0'
+	},
+
+    View1: {
+		borderColor: 'black',
+		borderWidth: 1,
+		height: '8%',
+		width: '99%',
+		backgroundColor: 'white',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 8,
+		marginTop: 16
+		
+
+    },
+
+	Text: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: '#101ed7'
+	},
+
+	Container2: {
+		flex: 1,
+		width: '50%'
+	}
+
+});
+
